31f922f011770768334f8fe02ee19da4f5b4c610
fuadinaqi <fuadinaqi@aisifoundation.org>
2018-02-10 22:05:35 +0700

    done

--- client/lib/index.js
+++ client/lib/index.js
@@ -10,6 +10,7 @@ function statusChangeCallback(response) {
       // localStorage.setItem('accessToken', response.authResponse.accessToken)
       testAPI();
     } else {
+      localStorage.clear()
       // The person is not logged into your app or we are unable to tell.
       document.getElementById('status').innerHTML = 'Please log ' +
         'into this app.';
